c7bc9b2796e3a36502347823b148023d3e938e76
rcrdgrc <ricktx04@gmail.com>
2019-05-24 16:11:42 -0500

    heroku

---
+++ models/appointment.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+// The factSchema is used to embedded docs in as tudent doc.
+// There is no model and no 'facts' collection
+
+// var userSchema = new mongoose.Schema({
+//   name: String,
+//   email: String,
+//   cohort: String,
+//   avatar: String,
+//   facts: [factSchema],
+//   googleId: String
+// }, {
+//   timestamps: true
+// });
+
+// module.exports = mongoose.model('User', studentSchema);
